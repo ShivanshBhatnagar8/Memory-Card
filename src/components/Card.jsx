f7ee41f721c5e639ba4def10718cd62ad7f48d49
Shivansh Bhatnagar <shivanshbhatnagar8@gmail.com>
2024-06-26 16:06:21 +0530

    To showcase cards in the ui

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+function Card({array, onclick}){
+    console.log(array);
+return(
+    <>
+    <div className="card-container">
+     {array.map((element,i) => (
+            <div key={i} className="card" name={element.name} onClick={()=> onclick(element.name)}>
+              <img src={`https://img.pokemondb.net/artwork/${element.name}.jpg`} alt={`${element.name} img`}/>
+              <p className="card-name">{element.name.toUpperCase()}</p>
+            </div>
+          ))}
+    </div>
+  </>
+)
+}
+export default Card;
