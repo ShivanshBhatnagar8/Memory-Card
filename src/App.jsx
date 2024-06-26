799799cf092911712144235837e8d97f85de24dd
Shivansh Bhatnagar <shivanshbhatnagar8@gmail.com>
2024-06-26 16:08:07 +0530

    Added functionality and changed the api to make game functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,80 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+import "./styles/style.css";
+import Card from "./components/Card";
+
+
 
 function App() {
-  // https://gateway.marvel.com:443/v1/public/characters?ts=1&limit=12&apikey=bd8b3d1ac0a7581457dfb795a9fc044b&hash=e84e4f9abc157ff276c22ac46df83fc3
-  let charactersId = [
-    1009220, 1009368, 1009664, 1009351, 1009338, 1009189, 1009282, 1009610,
-    1009707, 1009652, 1009685, 1009718,
-  ];
-  // const url = "https://gateway.marvel.com:443/v1/public/characters";
-  // const apiKey = "bd8b3d1ac0a7581457dfb795a9fc044b";
-  // const hash = "e84e4f9abc157ff276c22ac46df83fc3";
-  // const [arr, SetArr] = useState([]);
+  const [arr, SetArr] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+ 
+  async function fetchData() {
+    try{
+      let obj = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=12`);
+       let data = await obj.json();
+       let results = data.results.map((el)=>{
+         el.isClicked = false;
+         return el
+       })
+         SetArr(results);
+      }catch(error){
+        return error
+  }
+}
 
-  // async function fetchData() {
-  //   let obj = await fetch(
-  //     `${url}/1009220?ts=1&limit=12&apikey=${apiKey}&hash=${hash}`
-  //   );
-  //   let data = await obj.json();
-  //   console.log(data.data.results);
-  // }
-  // fetchData();
+  useEffect(()=>{
+  fetchData();
+  },[])
+  
+  function shuffleArray(array){
+   for(let i=0;i<array.length;i++){
+    let random = Math.floor(Math.random() * array.length);
+    [arr[random] , arr[i]] = [arr[i] , arr[random]];
+    }
+  }
+
+  function settingScores(elementName, name, clicked){
+   if(elementName === name){
+    if(clicked){
+    setBestScore(Math.max(score,bestScore));
+    setScore(0);
+    }else{
+      setScore(score+1);
+    }
+   }
+  }
+  function handleCardClick(name){
+    shuffleArray(arr);
+    
+   SetArr(arr.map((element)=>{
+    settingScores(element.name, name, element.isClicked);
+    if(score===arr.length){
+      setBestScore(score);
+      setScore(0);
+      element.isClicked = false;
+    }
+     if(element.name === name){
+     return {...element, isClicked: true};
+     }else{
+      return element;
+     }
+ }))
+}
 
   return (
     <>
-      <h1>Memory Game</h1>
-    </>
+    <header>
+      <h1>Memory Card</h1>
+      <div className="score-container">
+        <p>Score: {score}</p>
+        <p>Best Score: {bestScore}</p>
+      </div>
+      </header>
+     <Card array={arr} onclick={handleCardClick}></Card>
+     </>
   );
-}
 
+}
 export default App;
